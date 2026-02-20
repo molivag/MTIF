a35216c92fbe4df88c1ba0e46898f708882719ca
molivag FedOX <maoliva@cimne.upc.edu>
2026-02-20 16:15:30 +0100

    Nueva version de mtfi se agrega la instruccion install

--- mtfi.py
+++ mtfi.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 
-import argparse
 import subprocess
+import argparse
+import shutil
 import sys
 import os
+
+MAKETETRAMESH_URL = "https://github.com/yoshiya-usui/makeTetraMesh.git"
+TETGEN2FEMTIC_URL = "https://github.com/yoshiya-usui/TetGen2Femtic.git"
+MAKEMTR_URL_URL = "https://github.com/yoshiya-usui/makeMtr.git"
+MERGERESULT_URL = "https://github.com/yoshiya-usui/mergeResultOfFEMTIC.git"
+FEMTIC_URL = "https://github.com/yoshiya-usui/femtic.git"
+TETGEN_URL = "https://github.com/TetGen/TetGen.git"
 
 
 def read_config(config_file="mtif.conf"):
@@ -65,6 +73,55 @@ def run_post(args):
     )
 
 
+def check_tool(tool_name):
+    if shutil.which(tool_name) is None:
+        print(f"ERROR: Required tool '{tool_name}' not found in PATH.")
+        sys.exit(1)
+
+
+def check_environment():
+    print("== Checking required tools ==")
+
+    required_tools = ["git", "make"]
+
+    for tool in required_tools:
+        check_tool(tool)
+
+    # compilador
+    if shutil.which("gfortran") is None and shutil.which("ifort") is None:
+        print("ERROR: No Fortran compiler found (gfortran or ifort).")
+        sys.exit(1)
+
+    print("Environment OK.\n")
+
+def clone_repo(url, path):
+    if not os.path.exists(path):
+        print(f"Cloning {url} into {path}")
+        subprocess.run(["git", "clone", url, path], check=True)
+    else:
+        print(f"{path} already exists. Skipping clone.")
+
+    # shutil.copy(f"{path}dependencies/femtic/femtic", "bin/")
+
+
+def run_install():
+    check_environment()
+    os.makedirs("dependencies",exist_ok=True)
+    os.makedirs("bin", exist_ok=True)
+    if not os.path.exists("dependencies/femtic"):
+        subprocess.run(["git", "clone", FEMTIC_URL, "dependencies/femtic"], check=True)
+    subprocess.run(["patch", "Makefile", "../../patches/femtic.patch"],
+                   cwd="dependencies/femtic",
+                   check=True)
+    subprocess.run(["make"], cwd="dependencies/femtic", check=True)
+    shutil.copy("dependencies/femtic/femtic", "bin/")
+    print("FEMTIC installed correctly.")
+
+
+
+
+
+
 def main():
     parser = argparse.ArgumentParser(description="MTIF - Magnetotelluric Inversion Framework")
     subparsers = parser.add_subparsers(dest="command")
@@ -84,6 +141,8 @@ def main():
         run_inversion(config)
     elif args.command == "post":
         run_post(args.args)
+    elif args.command == "install":
+        run_install()
     else:
         parser.print_help()
 
