bf75c0fbc1bf3da836fd6b91d1385eee65f136ab
molivag <mogeofisico@gmail.com>
2026-05-08 14:03:41 +0200

    config - updating the las location of set.meshtran.io from bin to ./

--- src/mtif/config.py
+++ src/mtif/config.py
@@ -19,22 +19,26 @@ def update_meshtran_io_from_toml(config,mesh_type):
         "MESH_NATURE": mesh_type,
         "DEM_FILE": mesh_cfg["dem_file"],
         "DEM_UNITS": mesh_cfg["dem_units"],
+
         "XMIN_DOM": mesh_cfg["domain_x"][0],
         "XMAX_DOM": mesh_cfg["domain_x"][1],
         "YMIN_DOM": mesh_cfg["domain_y"][0],
         "YMAX_DOM": mesh_cfg["domain_y"][1],
         "ZMIN_DOM": mesh_cfg["domain_z"][0],
         "ZMAX_DOM": mesh_cfg["domain_z"][1],
+
         "PAD_X":mesh_cfg["pad_x"],
         "PAD_Y": mesh_cfg["pad_y"],
+
         "HAS_SEA":"YES" if mesh_cfg["has_sea"]else "NO",
         "SEA_LEVEL": mesh_cfg["sea_level"],
-        "ITER_TET_REFI": mesh_cfg["tet_refinement"],
         "TOPO_FILE": mesh_cfg["topo_file"],
         "BATHY_FILE": mesh_cfg["bathy_file"],
         "COSLI_FILE": mesh_cfg["coast_file"],
+
+        "ITER_TET_REFI": mesh_cfg["tet_refinement"],
     }
-    with open("bin/set_meshtran.io","r") as f:
+    with open("./set_meshtran.io","r") as f:
         lines = f.readlines()
     
     new_lines = []
@@ -53,7 +57,7 @@ def update_meshtran_io_from_toml(config,mesh_type):
 
         new_lines.append(line)
 
-    with open("bin/set_meshtran.io", "w") as f:
+    with open("./set_meshtran.io", "w") as f:
         f.writelines(new_lines)
 
     print(" 📖 set_meshtran.io updated from mtif.toml")
