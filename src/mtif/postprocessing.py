f2175672bf6b2ad6d2595b9300ae64bd16bf6d45
molivag <mogeofisico@gmail.com>
2026-05-12 11:53:09 +0200

    postprocessing - removing comments and unused lines

--- src/mtif/postprocessing.py
+++ src/mtif/postprocessing.py
@@ -15,21 +15,12 @@ def run_post(args):
     sites = args.sites if args.sites else post_cfg["default_sites"]
     iters = args.iter if args.iter else post_cfg["default_iter"]
     # iters = args.iter if args.iter is not None else post_cfg["default_iter"]
-    # procs = args.proc if args.proc else post_cfg["default_proc"]
     xaxis = args.axis if args.axis else post_cfg["default_axis"]
     comp = args.comp if args.comp else ["xy", "yx"]
 
     
-    # if args.mode is not None:
-    #     mode = args.mode                        #Si usuario pasa --mode impz → usa impz
-    # elif config.get("default_mode") in ["impz", "rhoph"]:
-    #     mode = config.get("default_mode")       #Si no pasa nada y el config tiene impz o rhoph → usa eso
-    # else:
-    #     mode = "none"                           #Si no hay nada válido → usa "none"
-
     results_path = f"{post_cfg['post_path']}/{job}"
     st_arg = f"st{sites}"
-    # ip_arg = f"ip{iters}-{procs}"
 
 
     print("   Results folder: ",results_path)
@@ -37,7 +28,6 @@ def run_post(args):
     print("   Sites:", sites)
     print("   Total Iterations:", iters)
 
-    # run(results_path,st_arg,ip_arg,xaxis, mode)
     run(results_path, iters, st_arg, xaxis,  comp)
 
 
